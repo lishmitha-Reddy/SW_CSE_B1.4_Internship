9c6df8cc34ae5edd67e3361cde370779981ac66f
lishmitha-Reddy <luckylishmitha098@gmail.com>
2026-07-29 11:13:26 +0530

    Hello World

--- Day/Day12/my-react-app/src/App.jsx
+++ Day/Day12/my-react-app/src/App.jsx
@@ -3,7 +3,7 @@ function App() {
   return (
     <div>
       <h1> Hello World </h1>
-      <h3> Welcome to my first REACT App.</h3>
+      <h3> Welcome to my first REACT App .</h3>
     </div>
 
   )
